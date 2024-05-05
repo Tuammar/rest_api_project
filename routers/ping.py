aa0ed92a88a17207a3b161fc85a35dd267ffb1d6
Tuammar <116462795+Tuammar@users.noreply.github.com>
2024-05-05 21:05:48 +0300

    Delete routers directory

--- routers/ping.py
+++
@@ -1,9 +0,0 @@
-from fastapi.routing import APIRouter
-
-from schemas import AnswerPing
-
-ping_router = APIRouter()
-
-@ping_router.get("/ping", response_model=AnswerPing)
-async def answer_ping() -> AnswerPing:
-    return {'test': 'ok'}
